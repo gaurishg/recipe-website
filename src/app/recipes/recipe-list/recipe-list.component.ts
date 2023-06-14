5b92d1c97e8c91e1e85a00193a64b4a0f464c0ac
Gaurish Gangwar <gaurishgangwar@gmail.com>
2023-06-14 08:01:20 +0000

    Passing data with event and property binding (upto 88 done)

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -8,8 +8,9 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent {
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is a test recipe', 'https://cdn.pixabay.com/photo/2015/08/25/03/50/herbs-906140_1280.jpg')
+    new Recipe('A Test Recipe', 'This is a test recipe', 'https://cdn.pixabay.com/photo/2015/08/25/03/50/herbs-906140_1280.jpg'),
+    new Recipe('Another Test Recipe', 'This is another test recipe', 'https://cdn.pixabay.com/photo/2015/08/25/03/50/herbs-906140_1280.jpg')
   ];
 
-  
+  @Output() recipeSelected = new EventEmitter<Recipe>();
 }
